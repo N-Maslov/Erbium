0d17563950715717e9361ba1ed9a5a12adebe102
Nick Maslov <nmaslov02@gmail.com>
2022-07-21 12:39:56 +0100

    v1: eliminated overflow errors

---
+++ 1d_eGPE.py
@@ -0,0 +1,88 @@
+import numpy as np
+import scipy.special as sc
+import ftransformer
+import warnings
+warnings.filterwarnings('error')
+# set parameters
+n = 1.0e19      # mean density
+L = 0.1         # length
+a_s = 3.0e-9    # contact length
+e_dd = 1        # interaction ratio
+m = 167.259*1.66e-27
+hbar = 1.055e-34# duh
+omegas = 2*np.pi*np.array([150,150,0]) # steepness of potential
+RES = 1000      # array length for integral and FFT
+
+# set wavefunction
+def psi_0(z):
+    return n**0.5*z**0
+
+# preliminary calculation
+g_s = 4*np.pi*hbar**2/m * a_s
+g_dd = g_s*e_dd
+gam_QF = 32/3 * g_s *(a_s**3/np.pi)**0.5 * (1+3/2*e_dd)
+N = n * L # number of particles
+step = L / RES # z increment
+zs = np.linspace(-L/2,L/2,RES,endpoint=False)
+psisq = np.abs(psi_0(zs))**2
+
+def particle_energy(eta,l):
+    """Calculate per-particle energy given wavefunction parameters:
+     - anisotropy eta
+     - mean width l.
+     Does not take vector inputs! Use energies_mat function for that."""
+    # preliminary calc
+    gam_sig = 2/(5*np.pi*1.5*l**3)
+    g_QF = gam_QF*gam_sig
+    # initialise with perpendicular energy contribution
+    val = hbar**2/(4*m*l**2)*(eta+1/eta) + m*l**2/4*(omegas[0]**2/eta+omegas[1]**2*eta)
+    
+    Phis = Phi(eta,l)
+    index = 0
+    for z in zs:
+        psi = psi_0(z) # wavefunction value to put into integrand
+        val += step/N*(
+            np.conjugate(psi) * (
+            (2/5*g_QF*np.abs(psi)**3 + 1/2*Phis[index].real + 1/2*m*omegas[2]**2*z**2)*psi
+            - hbar**2/(2*m) * (psi_0(z-step)-2*psi+psi_0(z+step))/step**2
+            )   
+        ) # get integrand at each point
+        index+=1
+    return val
+
+
+def Phi(eta,l):
+    F_psi_sq, ks = ftransformer.f_x4m(RES,L,psisq)
+    # drops redundant corresponding z's on return 
+    return ftransformer.inv_f_x4m(RES,ks[-1]-ks[0],U_sig(ks,eta,l)*F_psi_sq)[0]
+
+def U_sig(ks,eta,l):
+    Q_sqs = (1/2**0.5 * ks *eta**0.25 * l)**2
+    # calculate for limiting cases
+    numerat = np.zeros_like(ks)
+    for index, Q_sq in enumerate(Q_sqs):
+        if Q_sq == 0:
+            numerat[index] = 3.0
+        else:
+            try: # actual calculation
+                numerat[index] = 3*(Q_sq*np.exp(Q_sq)*sc.expi(-Q_sq)+1)
+            except Warning: # account for overflow error
+                pass # since zero already set
+    return g_s/(2*np.pi*l**2) + g_dd/(2*np.pi*l**2) * (numerat/(1+eta)-1)
+
+def energies_mat(etas,ls):
+    """Returns matrix of per-particle energies, each of which corresponds
+    to a separate (eta,l) pair."""
+    vals = np.zeros((len(etas),len(ls)))
+    for i in range(len(etas)):
+        for j in range(len(ls)):
+            vals[i,j] = particle_energy(etas[i],ls[j])
+    return vals
+
+### TESTING ###
+etas = np.linspace(0.5,1.5)
+ls = [0.01]
+energies = energies_mat(etas,ls)
+import matplotlib.pyplot as plt
+plt.plot(etas,energies[:,0])
+plt.show()
---
+++ ftransformer.py
@@ -0,0 +1,36 @@
+import numpy as np
+def f_x4m(N,L,in_vect):
+    """Approximates input as periodic sampled input.
+    Input vector has N entries between +-L/2.
+    Returns Fourier transformed array centred on zero to best match FT definition,
+    and corresponding k's."""
+    ks = 2*np.pi*np.fft.fftfreq(N,L/N)
+    x4m = L/N * np.exp(ks*L/2*1j) * np.fft.fft(in_vect)
+    x4m = np.fft.fftshift(x4m)
+    return x4m, np.fft.fftshift(ks)
+
+def inv_f_x4m(N,k_range,in_vect):
+    """Inverse fourier transform. Same format as f_x4m; returns corresponding x's."""
+    inv_input = in_vect[::-1]
+    x4m = f_x4m(N,k_range,np.concatenate(([0],inv_input[:-1])))
+    return 1/(2*np.pi)*x4m[0], x4m[1]
+
+### TESTING ###
+"""import matplotlib.pyplot as plt
+L = 100
+N = 10000
+zs = np.linspace(-L/2,L/2,N,endpoint=False,dtype=complex)
+
+func = np.exp(-np.abs(zs-10))
+transform, k_vals = f_x4m(N,L,func)
+# works up to here. now try inverse transform.
+og_func, x_vals = inv_f_x4m(N,k_vals[-1]-k_vals[0],transform)
+
+func = zs**2
+transform, k_vals = f_x4m(N,L,func)
+
+#plt.plot(k_vals,transform)
+og_func, x_vals = inv_f_x4m(N,k_vals[-1]-k_vals[0],transform)
+plt.plot(zs,func)
+plt.plot(x_vals, og_func)
+plt.show()"""
