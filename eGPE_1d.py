2160addb03d8acafe71dbbc066070a44ea4a793e
Nick Maslov <nmaslov02@gmail.com>
2022-07-22 11:45:31 +0100

    modified KE calc to use FFT

--- eGPE_1d.py
+++ eGPE_1d.py
@@ -1,8 +1,9 @@
 import numpy as np
 import scipy.special as sc
-import ftransformer
+import ftransformer as ft
 from scipy.optimize import minimize
 import warnings
+import cProfile
 warnings.filterwarnings('error')
 
 # set parameters
@@ -25,10 +26,14 @@ g_dd = g_s*e_dd
 gam_QF = 32/3 * g_s *(a_s**3/np.pi)**0.5 * (1+3/2*e_dd)
 N = n * L # number of particles
 step = L / RES # z increment
-zs = np.linspace(-L/2,L/2,RES,endpoint=False)
-psisq = np.abs(psi_0(zs))**2
 
-def particle_energy(eta,l):
+zs = np.linspace(-L/2,L/2,RES,endpoint=False)
+psis = psi_0(zs)
+psisq = np.abs(psis)**2
+F_psi = ft.f_x4m(RES,L,psis)[0]
+F_psi_sq, ks = ft.f_x4m(RES,L,psisq)
+
+def particle_energy(eta:float,l:float):
     """Calculate per-particle energy given wavefunction parameters:
      - anisotropy eta
      - mean width l.
@@ -38,7 +43,8 @@ def particle_energy(eta,l):
     g_QF = gam_QF*gam_sig
     # initialise with perpendicular energy contribution
     val = hbar**2/(4*m*l**2)*(eta+1/eta) + m*l**2/4*(omegas[0]**2/eta+omegas[1]**2*eta)
-    
+    # get kinetic energies for each point
+    KE_contribs = hbar**2/(2*m) * ft.inv_f_x4m(RES,L,F_psi*ks**2)[0].real
     Phis = Phi(eta,l)
     index = 0
     for z in zs:
@@ -46,19 +52,19 @@ def particle_energy(eta,l):
         val += step/N*(
             np.conjugate(psi) * (
             (2/5*g_QF*np.abs(psi)**3 + 1/2*Phis[index].real + 1/2*m*omegas[2]**2*z**2)*psi
-            - hbar**2/(2*m) * (psi_0(z-step)-2*psi+psi_0(z+step))/step**2
+            +KE_contribs[index]
             )   
         ) # get integrand at each point
         index+=1
     return val
 
 
-def Phi(eta,l):
-    F_psi_sq, ks = ftransformer.f_x4m(RES,L,psisq)
+def Phi(eta:float,l:float):
     # drops redundant corresponding z's on return 
-    return ftransformer.inv_f_x4m(RES,ks[-1]-ks[0],U_sig(ks,eta,l)*F_psi_sq)[0]
+    return ft.inv_f_x4m(RES,ks[-1]-ks[0],U_sig(ks,eta,l)*F_psi_sq)[0]
 
-def U_sig(ks,eta,l):
+def U_sig(ks:np.ndarray,eta:float,l:float):
+    """Calculate approximation function for 2D fourier transform."""
     Q_sqs = (1/2**0.5 * ks *eta**0.25 * l)**2
     # calculate for limiting cases
     numerat = np.zeros_like(ks)
@@ -73,7 +79,7 @@ def U_sig(ks,eta,l):
                 pass # since zero already set
     return g_s/(2*np.pi*l**2) + g_dd/(2*np.pi*l**2) * (numerat/(1+eta)-1)
 
-def energies_mat(etas,ls):
+def energies_mat(etas:np.ndarray,ls:np.ndarray):
     """Returns matrix of per-particle energies, each of which corresponds
     to a separate (eta,l) pair."""
     vals = np.zeros((len(etas),len(ls)))
@@ -86,3 +92,4 @@ energy_func = lambda x: particle_energy(x[0],x[1])*1.e26
 bnds = ((0.01,None),(1.e-9,None))
 res = minimize(energy_func,(1.1,0.001),bounds=bnds)
 print(res.x)
+#cProfile.run('')
